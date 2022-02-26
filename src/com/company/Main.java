9e1e48ab405f4e2911af70ce89d55e027f3e767f
meow-error <egorlevadniy2002@gmail.com>
2022-02-26 19:18:45 +0300

    Работа с книгой

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        XSSFWorkbook workbook = new XSSFWorkbook("C:\\Users\\user\\IdeaProjects\\homework2\\info\\condition.xlsx");
+
+        int number = 7; // Номер варианта
+        XSSFSheet sheet = workbook.getSheetAt(number - 1);
+
+        double array0[] = new double[sheet.getLastRowNum()];
+
+        int column = 0;
+        for (int i = 1; i <= sheet.getLastRowNum(); i++){
+            array0[i - 1] = sheet.getRow(i).getCell(column).getNumericCellValue();
+        }
+
+        workbook.close();
+
+        for (double line: array0) {
+            System.out.println(line);
+        }
+
+
+
+
+
     }
 }
